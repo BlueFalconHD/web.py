3ea65765ce46faa75fecfcdb509d05112a2d2b03
BlueFalconHD <72631767+BlueFalconHD@users.noreply.github.com>
2021-04-10 18:23:15 -0500

    Improved compileSite() function
    added DOCTYPE args and <body> & <head> elements.

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 
 
+
 def csscomp(args):
     full = []
     for i in range(len(args)):
@@ -12,7 +13,7 @@ def properties(args):
     for i  in range(len(args)):
         full.append(args[i] + " ")
     return(''.join(full))
-def element(css, type, content = "", props=[], runCompilecss = True):
+def element(type,css = [], content = "", props=[], runCompilecss = True):
     if runCompilecss == True:
         full = "<" + type + " " + properties(props) + 'style="' + csscomp(css) + '">' + content + "</" + type + ">"
     else:
@@ -21,7 +22,16 @@ def element(css, type, content = "", props=[], runCompilecss = True):
 def cssDark():
     return(csscomp(["background-color: rgb(68, 68, 68)", "padding-left: 20px", "margin-left: 25px", "color: rgb(190, 190, 190)", "text-shadow: 0 4px 2px -2px rgb(135, 134, 131)", "box-shadow: 0 4px 2px -2px rgb(135, 134, 131)"]))
 
-def compileSite(code, filename):
-    code = open(str(filename), 'r+')
-    code.write(code)
-    code.close()
+def compileSite(code=[], headcode=[], filename="index"):
+    codefile = open(str(filename) + ".html", 'a+')
+    codelist = []
+    for i in range(len(code)):
+        codelist.append(code[i])
+        codelist.append("\n")
+    headcodelist = []
+    for i in range(len(headcode)):
+        headcodelist.append(headcode[i])
+        headcodelist.append("\n")
+    codefile.write("<!DOCTYPE html>\n<html>\n<head>\n" + ''.join(headcodelist) + "\n</head>\n<body>\n" + ''.join(codelist) + "\n</body>\n</html>")
+    codefile.close()
+
